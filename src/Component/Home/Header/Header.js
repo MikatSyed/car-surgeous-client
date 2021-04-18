3dc305d585539d8ec5814e26e3f1c13c8f21bcb8
Mikat Syed <syednew5000@gmail.com>
2021-04-19 02:23:54 +0600

    my client server

--- src/Component/Home/Header/Header.js
+++ src/Component/Home/Header/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
             <FeatureService/>
             <Testimonial/>
             <Blog/>
-           <MakeAdmin/>
+          
           
           
             <Footer/>
